14384baded5b1e706067d0f0a628120922f3c3d2
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-03-19 21:14:07 +0530

    build: Project Setup

--- EmartX/src/main.jsx
+++ EmartX/src/main.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { AuthProvider } from "./Auth/firebase.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
